3435cd875c0f64a82a1c0a489d654f11cd93731e
Andre Campos <andrecampos@greenmile.com>
2016-04-23 15:42:02 -0300

    commit inicial boot strap: Web×JPA×MySQL×Facebook×LinkedIn×Twitter×

---
+++ src/main/java/com/diseasy/DiseasyApplication.java
@@ -0,0 +1,12 @@
+package com.diseasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiseasyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/diseasy/DiseasyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.diseasy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiseasyApplication.class)
+@WebAppConfiguration
+public class DiseasyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
