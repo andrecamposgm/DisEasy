86586c5735e139d06e5e5c5e5ad24c924bdbc891
Augusto Monteiro <augustomna2010@gmail.com>
2016-04-24 10:27:08 -0300

    map

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -13,16 +13,41 @@ Helpers.getLocation = function (callback) {
 
 Maps.MainMap = L.map('MainMap').setView([51.505, -0.09], 13); 
 
-Maps.GoogleLayer = new L.Google('ROADMAP'); 
+Maps.GoogleLayer = new L.Google('ROADMAP');
 
 Maps.MainMap.addLayer(Maps.GoogleLayer);
 
+Maps.Markers = [];
+
+Helpers.addDiseaseMarker = function (register) {
+    var icon = L.icon({
+        iconUrl: '/images/marker-' + register.color + '.png',
+        iconSize:     [32, 48],
+        iconAnchor:   [0, 0],
+        popupAnchor:  [0, 0]
+    });
+    var marker = L.marker([register.latitude, register.longitude], {icon: icon});
+    marker.bindPopup("<strong>Disease: </strong>" + register.disease.name + "<br>" +
+    "<strong>Infected Total: </strong>100<br>");
+    Maps.Markers.push(marker);
+};
+
 $(document).ready(function () {
-    $.get('http://ec2-52-91-172-65.compute-1.amazonaws.com//Diseases', function (diseases) {
+    $.get('http://ec2-52-91-172-65.compute-1.amazonaws.com/Diseases', function (diseases) {
         var el = $('#DiseaseSelect');
         $.each(diseases, function(a, disease) {
             el.append($("<option></option>")
                 .attr("value", disease.id).text(disease.name));
         });
     });
+
+    $.get('http://ec2-52-91-172-65.compute-1.amazonaws.com/DiseaseRegisters', function (registers) {
+        $.each(registers, function(a, register) {
+            Helpers.addDiseaseMarker(register);
+        });
+
+        var group = L.featureGroup(Maps.Markers).addTo(Maps.MainMap);
+        Maps.MainMap.fitBounds(group.getBounds());
+    });
+
 });
